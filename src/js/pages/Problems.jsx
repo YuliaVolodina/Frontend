d89ba3a868b095a74d0f4942034fd69b1f7ccaf7
GuillaumeLam <guillaume.lam10100@gmail.com>
2018-02-10 00:49:32 -0500

    Added a textarea and a submit button to problems page.

--- src/js/pages/Problems.jsx
+++ src/js/pages/Problems.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 
 export default class Problems extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Problems</h1>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "Please write codes"
+        };
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert("A solution code has been submitted: " + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <h1>Problems</h1>
+                </div>
+                <textarea value={this.state.value} onChange={this.handleChange}/>
+                <input type="submit" value="Submit" />
+            </form>
+
+        );
+    }
 }
