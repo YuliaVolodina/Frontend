2ad7655d5fee8e6210fdd63321e393aec15d9356
laurentpd <laurent.poulin-dube@mail.mcgill.ca>
2018-02-09 19:10:49 -0500

    help page info filled in

--- src/js/pages/Help.jsx
+++ src/js/pages/Help.jsx
@@ -3,10 +3,15 @@ import React from "react";
 
 export default class Help extends React.Component {
     render() {
-
         return (
             <div>
-                <h1>Help me</h1>
+                <h1>Help</h1>
+                <p> Welcome to Time Machine, where learning code is simple and fun.<br/>
+                    Get started by logging in, then select a problem to try and solve.<br/>
+                    Make sure to check the difficulty and to visit our tutorial page <br/>
+                    for extra help. For more questions, communicate with the System <br/>
+                    Administrator at <i>admin@timemachine.com</i>
+                </p>
             </div>
         );
     }
