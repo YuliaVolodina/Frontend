5ce493c2981a4fd3a6c9384c62980907725d4d63
laurentpd <laurent.poulin-dube@mail.mcgill.ca>
2018-02-11 12:03:38 -0500

    attempt at problem list

--- src/js/pages/Help.jsx
+++ src/js/pages/Help.jsx
@@ -10,7 +10,7 @@ export default class Help extends React.Component {
                     Get started by logging in, then select a problem to try and solve.<br/>
                     Make sure to check the difficulty and to visit our tutorial page <br/>
                     for extra help. For more questions, communicate with the System <br/>
-                    Administrator at <i>admin@timemachine.com</i>
+                    Administrator at <b>admin@timemachine.com</b>
                 </p>
             </div>
         );
--- src/js/pages/Problems.jsx
+++ src/js/pages/Problems.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 
 export default class Problems extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Problems</h1>
-      </div>
-    );
-  }
+    options = {
+        set0: ['Option 1', 'Option 2'],
+        set1: ['First Option', 'Second Option', 'Third Option']
+    };
+    displayList(array) {
+        let list = document.createElement('ul');
+        for(let i = 0; i < array.length; i++) {
+            // Create the list item:
+            let item = document.createElement('li');
+            // Set its contents:
+            item.appendChild(document.createTextNode(array[i]));
+            // Add it to the list:
+            list.appendChild(item);
+        }
+        // Finally, return the constructed list:
+        return list;
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Problems</h1>
+                <p id="demo"></p>
+                <script>{
+                    document.getElementById("demo").appendChild(
+                        this.displayList(this.options.set0)
+                    )
+                }</script>
+            </div>
+        );
+    }
 }
