6f03158712a285ee9235114c121a45ca342d71ad
edward-son <edward_son@live.com>
2018-02-12 01:21:00 -0500

    added filter module

--- src/js/pages/Problems.jsx
+++ src/js/pages/Problems.jsx
@@ -4,6 +4,7 @@ import { IndexLink, Link } from "react-router";
 
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
+import Select from 'react-select';
 
 import List from "../components/ProblemList.jsx";
 
@@ -39,43 +40,32 @@ export default class Problems extends React.Component {
         const { query } = this.props.location;
         const { params } = this.props;
         const { article } = params;
-        const { date, filter } = query;
         const createProblemClass = location.pathname.match(/^\/createProblem/) ? "active" : "";
 
-        // const Problems = [
-        //     {
-        //         name: "merge sort",
-        //         author: "Billy Joel",
-        //         description: "its fast",
-        //         difficulty: null,
-        //         good: null,
-        //         id: 1
-        //     },
-        //     {
-        //         name: "quick sort",
-        //         author: "Scooby Doo",
-        //         description: "its quick",
-        //         difficulty: null,
-        //         good: null,
-        //         id: 2
-        //     },
-        //     {
-        //         name: "radix sort",
-        //         author: "X",
-        //         description: "its rad",
-        //         difficulty: null,
-        //         good: null,
-        //         id: 3
-        //     },
-        //     {
-        //         name: "insertion sort",
-        //         author: "Y",
-        //         description: "its slow",
-        //         difficulty: null,
-        //         good: null,
-        //         id :4
-        //     }
-        // ].map((problem, i) => <List key={i} problem={problem}/> );
+
+        const options = [
+            'one', 'two', 'three'
+        ];
+        const defaultOption = options[0];
+
+        const { selectedOption } = this.state;
+        const value = selectedOption && selectedOption.value;
+        const filtered = [];
+
+        const filter = () => {
+            for(let problems of Problems){
+                if(selectedOption.label.equals("Difficulty")) {
+                    if (problems.difficulty.equals(selectedOption)) {
+                        filtered.push(options);
+                    }
+                }
+                if(selectedOption.label.equals("Rating")){
+                    if (problems.good.equals(selectedOption)) {
+                        filtered.push(options);
+                    }
+                }
+            }
+        }
 
 
         return (
@@ -85,6 +75,16 @@ export default class Problems extends React.Component {
                 <a  className={createProblemClass}>
                     <Link class="btn btn-success" to="createProblem">Add Problem</Link>
                 </a>
+                <Select
+                    name="form-field-name"
+                    value={value}
+                    onChange={this.handleChange}
+                    options={[
+                        { value: 'Difficulty', label: 'Difficulty' },
+                        { value: 'Rating', label: 'Rating' },
+                    ]}
+                />
+                <div class="row">{Problems}</div>
             </div>
         );
     }
