dc8d753051ab0f31ed0e49fe43d7e4b47d0e40c6
edward-son <edward_son@live.com>
2018-02-12 01:21:00 -0500

    added filter module

--- src/js/pages/Problems.jsx
+++ src/js/pages/Problems.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import Select from 'react-select';
 
 import List from "../components/ProblemList.jsx";
 
 export default class Problems extends React.Component {
+
+    state = {
+        selectedOption: '',
+    }
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+        console.log(`Selected: ${selectedOption.label}`);
+    }
+
+
     render() {
         const { query } = this.props.location;
         const { params } = this.props;
         const { article } = params;
-        const { date, filter } = query;
 
         const Problems = [
             {
@@ -44,9 +54,43 @@ export default class Problems extends React.Component {
             }
         ].map((problem, i) => <List key={i} problem={problem}/> );
 
+        const options = [
+            'one', 'two', 'three'
+        ];
+        const defaultOption = options[0];
+
+        const { selectedOption } = this.state;
+        const value = selectedOption && selectedOption.value;
+        const filtered = [];
+
+        const filter = () => {
+            for(let problems of Problems){
+                if(selectedOption.label.equals("Difficulty")) {
+                    if (problems.difficulty.equals(selectedOption)) {
+                        filtered.push(options);
+                    }
+                }
+                if(selectedOption.label.equals("Rating")){
+                    if (problems.good.equals(selectedOption)) {
+                        filtered.push(options);
+                    }
+                }
+            }
+        }
+
+
         return (
             <div>
                 <h1>Problems</h1>
+                <Select
+                    name="form-field-name"
+                    value={value}
+                    onChange={this.handleChange}
+                    options={[
+                        { value: 'Difficulty', label: 'Difficulty' },
+                        { value: 'Rating', label: 'Rating' },
+                    ]}
+                />
                 <div class="row">{Problems}</div>
             </div>
         );
