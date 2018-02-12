dcad8a6d89362e66da8cfced1f536e525828a700
GuillaumeLam <guillaume.lam10100@gmail.com>
2018-02-12 02:16:42 -0500

    Linked the frontend with the backend, problems are now linked to the backend, had to import axios and do some cross platform stuff

--- src/js/pages/Problems.jsx
+++ src/js/pages/Problems.jsx
@@ -1,53 +1,79 @@
 import React from "react";
+import axios from "axios";
+
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
 
 import List from "../components/ProblemList.jsx";
 
 export default class Problems extends React.Component {
+    state = {
+        Problems: []
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:8000/problems/")
+            .then(response => {
+                console.log(response);
+                const Problems = response.data.map((ent) => {
+                    ent["difficulty"] = null;
+                    ent["good"] = null;
+                    return ent
+                }).map((problem, i) => <List key={i} problem={problem}/> );
+                this.setState({Problems})
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
     render() {
         const { query } = this.props.location;
         const { params } = this.props;
         const { article } = params;
         const { date, filter } = query;
 
-        const Problems = [
-            {
-                name: "merge sort",
-                author: "Billy Joel",
-                description: "its fast",
-                difficulty: null,
-                good: null,
-                id: 1
-            },
-            {
-                name: "quick sort",
-                author: "Scooby Doo",
-                description: "its quick",
-                difficulty: null,
-                good: null,
-                id: 2
-            },
-            {
-                name: "radix sort",
-                author: "X",
-                description: "its rad",
-                difficulty: null,
-                good: null,
-                id: 3
-            },
-            {
-                name: "insertion sort",
-                author: "Y",
-                description: "its slow",
-                difficulty: null,
-                good: null,
-                id :4
-            }
-        ].map((problem, i) => <List key={i} problem={problem}/> );
+
+        // const Problems = [
+        //     {
+        //         name: "merge sort",
+        //         author: "Billy Joel",
+        //         description: "its fast",
+        //         difficulty: null,
+        //         good: null,
+        //         id: 1
+        //     },
+        //     {
+        //         name: "quick sort",
+        //         author: "Scooby Doo",
+        //         description: "its quick",
+        //         difficulty: null,
+        //         good: null,
+        //         id: 2
+        //     },
+        //     {
+        //         name: "radix sort",
+        //         author: "X",
+        //         description: "its rad",
+        //         difficulty: null,
+        //         good: null,
+        //         id: 3
+        //     },
+        //     {
+        //         name: "insertion sort",
+        //         author: "Y",
+        //         description: "its slow",
+        //         difficulty: null,
+        //         good: null,
+        //         id :4
+        //     }
+        // ].map((problem, i) => <List key={i} problem={problem}/> );
+
 
         return (
             <div>
                 <h1>Problems</h1>
-                <div class="row">{Problems}</div>
+                <div class="row">{this.state.Problems}</div>
             </div>
         );
     }
