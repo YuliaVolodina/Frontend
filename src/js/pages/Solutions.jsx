194320fdf0f46ac02307b62cdc13731a99e6af87
edward-son <edward_son@live.com>
2018-02-13 18:48:03 -0500

    handle text input event for solutions

--- src/js/pages/Solutions.jsx
+++ src/js/pages/Solutions.jsx
@@ -6,24 +6,35 @@ export default class Solutions extends React.Component {
     problem = [];
     problem = this.props.location.state.testvalue;
 
-    handleChange(event){
-        console.log(event);
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(event){
+    handleChange(event) {
+        this.setState({value: event.target.value});
+        console.log(this.state);
+    }
 
+    handleSubmit(event) {
+        console.log("hello");
+        alert('A name was submitted: ' + this.state.value);
+        console.log(this.state.value);
+        event.preventDefault();
     }
 
     render() {
-        console.log(this.problem);
 
         return(
             <div>
               <h1>{this.problem.name}</h1>
                 <p>{this.problem.description}</p>
                 <p>Enter solution in the box below</p>
-                <Textarea style = {{width:900, height: 300}} onChange={this.handleChange()}/>
-                <a class="btn -btn-default" onChange={this.handleSubmit}>Submit</a>
+                <Textarea style = {{width:900, height: 300}} onChange={this.handleChange}/>
+                <a class="btn -btn-default" onClick={this.handleSubmit}>Submit</a>
             </div>
         );
     }
