76efaa56c189c47fbb134591b08fdcec2ccb2139
Justin <junyuu@hotmail.com>
2018-02-14 18:51:51 -0500

    added cm's github button

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
+ function confirmBox(){
+  	var text;
+  	if (confirm("Logged in with GitHub!")) {
+  		text = "Cool";
+  	} else {
+  		text = "not cool";
+  	}
+  }
 
 export default class Home extends React.Component {
   render() {
-
     return (
       <div>
-        <h1>Home</h1>
+        <h1>
+        	Home
+        </h1>
+        <button onClick={confirmBox()}>
+        	Log in with Github
+        </button>
       </div>
     );
   }
