bbf387b5a8e13695a7439a9663a384687c674606
Justin <junyuu@hotmail.com>
2018-03-11 18:18:40 -0400

    attempt at snapshot testing

--- __tests__/filter.test.jsx
+++ __tests__/filter.test.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import ReactTestUtils from 'react-dom/test-utils';
-import renderer from 'react-test-renderer';
 import Problems from '../src/js/pages/Problems.jsx';
-import { shallow } from 'enzyme';
-import * as enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import renderer from 'react-test-renderer';
 
-//tslint:disable-next-line:no-any
-(enzyme).configure({ adapter: new Adapter() });
-
-it('filtered expert correctly', () =>{
-	const component = shallow(<Problems />);
-	component.setState({selectedOption: 'Expert'});
-	expect(component.find(Problems).length).toBe(1);
-});
+test('Problems page information correct',() =>{
+	const component = renderer.create(
+ 		<Problems />,
+	);
+	let tree = component.toJSON();
+ 	expect(tree).toMatchSnapshot();
+})
